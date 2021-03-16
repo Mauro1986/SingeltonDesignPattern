17891ddc15af0e3b5f80c3b3d0c774808684c676
MAURO GAY <MAURO.GAY@student.intecbrussel.be>
2021-03-16 10:30:45 +0100

    Add project files.

---
+++ SingeltonDesignPattern/Models/EmployeeServices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonDesignPattern.Models
+{
+    public class EmployeeServices
+    {
+
+        private static EmployeeServices instance;
+        private List<Employee> employeeList = null;
+
+        private static readonly object _lock = new object();
+
+        private EmployeeServices()                        //bij Singelton is constructor PRIVATE(grootste verschil)
+        {
+            if (employeeList == null)
+            {
+                employeeList = new List<Employee>();
+            }
+        }
+
+        public static EmployeeServices Instance()           // make instance only if instance is NULL
+        {
+            lock (_lock)                                    //lock nodig voor threadSafety!
+            {
+                if (instance == null)
+                {
+                    instance = new EmployeeServices();
+                }
+                return instance;
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            employeeList.Add(employee);
+        }
+
+        public List<Employee> GetEmployee(string firstname)
+        {
+           return employeeList.FindAll(e => e.FirstName.Contains(firstname));
+        }
+
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeList;
+        }
+    }
+}
---
+++ SingeltonDesignPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿using SingeltonDesignPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingeltonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee0 = new Employee {FirstName="Kenan", LastName="Kurda" };
+            Employee employee1 = new Employee {FirstName="Mauro", LastName="Gay" };
+            Employee employee2 = new Employee {FirstName="Timothy", LastName="Hsu" };
+            Employee employee3 = new Employee {FirstName="Toon", LastName="Penen" };
+            Employee employee4 = new Employee {FirstName="Francis", LastName="Bulu" };
+            Employee employee5 = new Employee {FirstName="Emre", LastName="Elagoz" };
+
+            EmployeeServices employeeServices = EmployeeServices.Instance();
+
+            employeeServices.AddEmployee(employee0);
+            employeeServices.AddEmployee(employee1);
+            employeeServices.AddEmployee(employee2);
+            employeeServices.AddEmployee(employee3);
+            employeeServices.AddEmployee(employee4);
+            employeeServices.AddEmployee(employee5);
+
+            Employee e1 = new Employee { FirstName = "Mr", LastName = "Bean" };
+
+            EmployeeServices employeeServices177777 = EmployeeServices.Instance();
+            employeeServices177777.AddEmployee(e1);
+
+            //var items = employeeServices.GetEmployee("Toon");
+
+            //foreach (var item in items)
+            //{
+            //    Console.WriteLine($"First Name {item.FirstName}  Last Name: {item.LastName}");
+            //}
+
+            var allEmployees = employeeServices.GetAllEmployees();
+
+            foreach (var item in allEmployees)
+            {
+                Console.WriteLine($"FirstName: {item.FirstName.PadRight(10)} LastName: {item.LastName.PadRight(25)}");
+            }
+        }
+    }
+}
